6d82c9e600f08bb58042f827c2609c53f5e7f0bf
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-24 14:59:09 +0200

    Revert "step 3 and 4"
    This reverts commit 6ade64b40ffdac962b3aa7a9a6df9c310cde18a2.

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
-
-button.onclick= function(){
-    let insideTheInput = input.value;
-       input.value = " ";
-}   
