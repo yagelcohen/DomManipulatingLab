6ade64b40ffdac962b3aa7a9a6df9c310cde18a2
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-24 14:58:51 +0200

    step 3 and 4

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
+
+button.onclick= function(){
+    let insideTheInput = input.value;
+       input.value = " ";
+}   
