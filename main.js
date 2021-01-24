2f57d4e0852e2a8ccf1c2e2356efcb0a3ec2a40c
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-24 15:05:30 +0200

    step 6-9

--- main.js
+++ main.js
@@ -5,4 +5,15 @@ const button = document.querySelector('button');
 button.onclick= function(){
     let insideTheInput = input.value;
        input.value = " ";
-  }   
+     
+  
+  const listItem = document.createElement('li');
+  const span = document.createElement('span');
+  const button2 = document.createElement('button');
+  
+  listItem.append(span);
+  span.innerText = insideTheInput;
+  listItem.append(button2);
+  button2.innerText='Delete';
+  list.append(listItem);
+}
